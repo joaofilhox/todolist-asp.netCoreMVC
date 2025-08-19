59ce171d81304aaf08081eae7eb4dbf3d5a0fc77
João Filho <joaofilholimati@gmail.com>
2025-08-19 16:18:48 -0300

    feat: Enviando dados do Controller para a View com ViwData, ViewBag e TempData

--- TWTodoList/TWTodoList/Controllers/TestController.cs
+++ TWTodoList/TWTodoList/Controllers/TestController.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TWTodoList.Models;
 
 namespace TWTodoList.Controllers;
 
 public class TestController : Controller
 {
     public IActionResult Index()
+    {
+        // Formas de passar dados da Controller para a View
+        //ViewData["apresentacao"] = "Hello, World!";
+        //ViewData.Add("apresentacao", "Hello World Update!");
+
+        var todo = new Todo         {
+            Title = "Estudar ASP.NET Core",
+            Description = "Estudar ASP.NET Core MVC e Razor Pages"
+        };
+        
+        //ViewData["todo"] = todo;
+        ViewBag.Todo = todo;
+
+        TempData["message"] = "Mensagem de teste com TempData";
+        return View();
+    }
+    public IActionResult Message()
     {
         return View();
     }
---
+++ TWTodoList/TWTodoList/Views/Test/Message.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TWTodoList.Views.Test
+{
+    public class MessageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
