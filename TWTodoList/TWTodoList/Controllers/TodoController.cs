fa55ec5f4c617eb81bb199010a9063c0166d1683
João Filho <joaofilholimati@gmail.com>
2025-08-20 09:56:30 -0300

    feat: Criando a Action de listagem de Tarefas

---
+++ TWTodoList/TWTodoList/Controllers/TodoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TWTodoList.Contexts;
+
+namespace TWTodoList.Controllers;
+
+public class TodoController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public TodoController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var todos = _context.Todos.ToList();
+        var viewModel = new ViewModels.ListTodoViewModel
+        {
+            Todos = todos
+        };
+        ViewData["Title"] = "Lista de Tarefas";
+        return View(viewModel);
+    }
+}
